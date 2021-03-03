229ea042c55c4c32b8d7324e97e7aa23376ff7eb
owl <baranovsky0803@gmail.com>
2021-03-03 11:22:58 +0300

    Added constant to Task_12

--- src/com/gmail/baranovsky0803/additional_tasks/Task_12.java
+++ src/com/gmail/baranovsky0803/additional_tasks/Task_12.java
@@ -5,14 +5,15 @@ package com.gmail.baranovsky0803.additional_tasks;
  */
 
 public class Task_12 {
-    public static void main(String[] args) {
 
-        //  Переменная для хранения значения дюйма
-        double inch = 2.54;
+    //  Статическая переменная для хранения значения дюйма
+    private static final double INCH_TO_CENTIMETER_MULTIPLIER = 2.54;
+
+    public static void main(String[] args) {
 
         // Вывод таблицы
         for (int i = 1; i <= 20; i++) {
-            System.out.println(i + "(в дюймах) = " + (i * inch) + "(в см)");
+            System.out.println(i + "(в дюймах) = " + (i * INCH_TO_CENTIMETER_MULTIPLIER) + "(в см)");
         }
     }
 
